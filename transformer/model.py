93b68c1cf7f09de1424d3a54443800f937380f81
Rodrigo Gomes Dutra <dutra.rgdgd@gmail.com>
2021-08-05 17:01:01 -0300

    Organize transformer code

---
+++ transformer/model.py
@@ -0,0 +1,182 @@
+import torch
+import math
+from torch import nn
+
+
+class PositionalEncoding(nn.Module):
+    """Vanilla PositionalEncoding encoding
+
+    According to the Attention is all you need paper, this positional
+    encoding inserts the notion of position in the input features.
+
+    Args:
+        d_model : size of entry feature array
+        max_len : Array of objects with simulation data
+    """
+    def __init__(self, d_model, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() *
+            (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return x + self.pe[:x.size(0), :]
+
+
+class TransformerTimeSeries(nn.Module):
+    """Transformer model adapted to time series data
+
+    This model follows the implementation from [1], which adapted the original
+    model from [2] to perform a time series prediction task instead of the original
+    task of NLP. The adaptation to the time series task removed the input
+    embedding from the encoder but maintained the positional encoding process.
+
+    References:
+    [1] Vaswani, Ashish, et al. "Attention is all you need." Advances in neural
+        information processing systems. 2017.
+    [2] Wu, Neo, et al. "Deep transformer models for time series forecasting: The influenza
+        prevalence case." arXiv preprint arXiv:2001.08317 (2020).
+
+    Args:
+        device            : Torch device to process the model
+        encoder_vector_sz : Size of the encoder input vector, which also define the
+                            number the encoder's input features.
+        decoder_vector_sz : Size of the decoder input vector, which also define the
+                            number the decoder's input features.
+        num_layers        : Number of decoder and encoder layers.
+        n_time_steps      : Number of timesteps used as inputs in the encoder and
+                            decoder.
+        d_model           : Model's vector dimmension.
+        max_len : Array of objects with simulation data
+
+    """
+    def __init__(self,
+                 device,
+                 encoder_vector_sz=1,
+                 decoder_vector_sz=1,
+                 num_layers=1,
+                 n_time_steps=30,
+                 d_model=512,
+                 dropout=0.1):
+        super(TransformerTimeSeries, self).__init__()
+        self.model_type = 'Transformer'
+        self.device = device
+
+        # Mask to previne look ahead behavior in the decoder process
+        self.mask = self._generate_square_subsequent_mask(n_time_steps)
+        # Positional encoding used in the decoder and encoder
+        self.pos_encoder = PositionalEncoding(d_model)
+        self.pos_decoder = PositionalEncoding(d_model)
+
+        # Encoder and encoder layers
+        # Note that this implementation follows the data input format of
+        # (batch, timestamps, features).
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model,
+                                                        nhead=2,
+                                                        dropout=dropout,
+                                                        batch_first=True)
+        self.decoder_layer = nn.TransformerDecoderLayer(d_model=d_model,
+                                                        nhead=2,
+                                                        dropout=dropout,
+                                                        batch_first=True)
+
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer,
+                                                         num_layers=num_layers)
+        self.transformer_decoder = nn.TransformerDecoder(self.decoder_layer,
+                                                         num_layers=num_layers)
+
+        # Encoder and decoder input projectors
+        self.encoder_projection = nn.Linear(encoder_vector_sz, d_model)
+        self.decoder_projection = nn.Linear(decoder_vector_sz, d_model)
+
+        # Transformer output layer
+        # Note that the output format of this model is (batch, pred_steps), where
+        # as a time series predictor the pred_steps contain the estimation of future
+        # behavior of one variable in time.
+        self.linear = nn.Linear(d_model, 1)
+        self.init_weights()
+
+    def _generate_square_subsequent_mask(self, sz):
+        """Generates decoder mask
+
+        This mask prevents the look ahead behavior in the decoder process.
+
+        Args:
+            sz : Size of decoder mask.
+        """
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(
+            mask == 1, float(0.0))
+        return mask
+
+    def encoder_process(self, src):
+        """Encoder process of the Transformer network.
+
+        According to [1], the tranformer encoder produces an encoded version
+        of the entry series with the positional information and a self learned
+        projection of the input data, generating a encoder matrix of dmodel
+        dimension.
+
+        Args:
+            src: Input data of  the encoder, with shape
+                 (batch, n_time_steps, encoder_vector_sz)
+        """
+
+        src_start = self.encoder_projection(src)
+        pos_encoded = self.pos_encoder(src)
+        src = pos_encoded + src_start
+        src = self.transformer_encoder(src) + src_start
+
+        return src
+
+    def decoder_process(self, tgt, memory):
+        """Decoder process of the Transformer network.
+
+        The tranformer decoder process the encoder's output, which is the
+        encoder's representation of the input, and the decoder's input
+        to estimate the future values a particular feature.
+
+
+        Args:
+            tgt    : Decoder input, which is previous values of the target
+                     features, and depending on the way used to process information
+                     this can also be previous outputs of the transformer network.
+
+            memory : Memory, that is the encoder representation of its input.
+        """
+        tgt_start = self.decoder_projection(tgt)
+        pos_decoder = self.pos_decoder(tgt)
+        tgt = pos_decoder + tgt_start
+        decoder_mask = self.mask.to(self.device)
+        out = self.transformer_decoder(tgt=tgt,
+                                       memory=memory,
+                                       tgt_mask=decoder_mask)
+        out = self.linear(out)
+
+        return out
+
+    def init_weights(self):
+        """Initialize the output layer weights.
+        """
+        initrange = 0.1
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, x):
+        """Send the information through the transformer network.
+        """
+
+        src, tgt = x
+        src = self.encoder_process(src)
+        out = self.decoder_process(tgt, src)
+
+        # Reshapes the output to (batch, n_pred_steps)
+        out = torch.reshape(out, (-1, out.shape[1]))
+        return out
---
+++ transformer/utils.py
@@ -0,0 +1,83 @@
+import time
+import torch
+from torch.utils.data import Dataset
+from torch.autograd import Variable
+
+
+class TransformerSet(Dataset):
+    """Transformer dataset object
+
+   This dataset object ensures that the transformer network is
+   feed correctly and also saves the true targets values for
+   posterior evaluation.
+
+    Args:
+        x_matrix : Matrix containing the past steps of a univariate
+                   time series. With shape (time_steps, window_of_features)
+
+        y_matrix : Matrix containing the future steps of a univariate time series.
+                   With shape (time_steps, window_of_features)
+
+        n_time_steps: Number of timesteps used in the entry of the transformer.
+    """
+    def __init__(self, x_matrix, y_matrix, n_time_steps):
+
+        x_matrix = x_matrix.reshape(-1, n_time_steps, 1)
+        n_encoder_input_steps = n_time_steps // 2
+        self.encoder_input = x_matrix[:, :n_encoder_input_steps]
+        self.decoder_input = x_matrix[:, n_encoder_input_steps:]
+        self.true_target = y_matrix
+
+    def __len__(self):
+        return len(self.encoder_input)
+
+    def __getitem__(self, idx):
+        return self.encoder_input[idx], self.decoder_input[
+            idx], self.true_target[idx]
+
+
+def batch_train(model, epoch, batch_size, train_loader, criterion, optimizer,
+                scheduler, set_size, device):
+    """Train the model in batch
+    """
+    model.train()  # Turn on the train mode
+    batch_loss = 0.
+    total_loss = 0.
+    start_time = time.time()
+    predictions = torch.tensor([]).to(device)
+    ground_truth = torch.tensor([]).to(device)
+
+    for i, batch in enumerate(train_loader):
+        src, tgt_in, tgt_out = batch[0], batch[1], batch[2]
+        src = Variable(torch.Tensor(src.float())).to(device)
+        tgt_in = Variable(torch.Tensor(tgt_in.float())).to(device)
+        tgt_out = Variable(torch.Tensor(tgt_out.float())).to(device)
+
+        optimizer.zero_grad()
+        output = model((src, tgt_in))
+        loss = criterion(output, tgt_out)
+        loss.backward()
+        scheduler.step()
+
+        batch_loss += loss.item()
+        total_loss += batch_loss
+        log_interval = int(set_size / batch_size / 5)
+        if i % log_interval == 0 and i > 0:
+            cur_loss = batch_loss / log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | '
+                  'lr {:02.6f} | {:5.2f} ms | '
+                  'loss {:5.5f}'.format(epoch, i, set_size // batch_size,
+                                        scheduler.get_lr()[0],
+                                        elapsed * 1000 / log_interval,
+                                        cur_loss))
+            batch_loss = 0
+            start_time = time.time()
+        pred = output
+        predictions = torch.cat((predictions, pred), 0)
+        ground_truth = torch.cat((ground_truth, tgt_out), 0)
+
+    predictions = predictions.cpu().detach().numpy()
+    ground_truth = ground_truth.cpu().cpu().detach().numpy()
+
+    return total_loss
