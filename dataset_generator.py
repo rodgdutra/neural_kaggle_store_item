9c9016043dedd7760cea2b44dd77ffae2df37956
Rodrigo Gomes Dutra <dutra.rgdgd@gmail.com>
2022-02-22 14:51:47 -0300

    Update dataset generator

--- dataset_generator.py
+++ dataset_generator.py
@@ -16,19 +16,21 @@ def synthetic_data(Nt=2000, tf=80 * np.pi):
 
 
 def main():
-    prediction_horizon = 30
+    prediction_horizon = 200
     val_size = 300
     total_train = 1500
     total = total_train + prediction_horizon + val_size
-
     simple = True
-
     if simple:
+        t = 10
+        fs = 300
+        samples = np.linspace(0, t, int(fs*t), endpoint=False)
+        f = 12
         time_idx = np.arange(total) / total * 2 * np.pi
-        data_train = np.sin(2 * np.pi * 4 * time_idx)[:val_size + total_train]
-        data_val = np.sin(2 * np.pi * 4 * time_idx)[total_train:total_train +
-                                                    val_size]
-        data_test = np.sin(2 * np.pi * 4 * time_idx)[total_train + val_size:]
+        data_train = np.sin(2 * np.pi * f * samples)[:val_size + total_train]
+        data_val = np.sin(2 * np.pi * f * samples)[total_train:total_train +
+                                                   val_size]
+        data_test = np.sin(2 * np.pi * f * samples)[total_train + val_size:]
 
     else:
         t, data_total = synthetic_data(Nt=total)
@@ -45,9 +47,9 @@ def main():
     df_test = {'data': data_test}
     df_test = pd.DataFrame(df_test)
 
-    df_train.to_csv("train_series.csv")
-    df_val.to_csv("val_series.csv")
-    df_test.to_csv("test_series.csv")
+    df_train.to_csv("train_series_2.csv")
+    df_val.to_csv("val_series_2.csv")
+    df_test.to_csv("test_series_2.csv")
 
 
 if __name__ == '__main__':
